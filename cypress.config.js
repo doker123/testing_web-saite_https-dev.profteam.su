51772c90462d142c8e49508deebd2d5df8a44820
doker123 <dodo2larin@gmail.com>
2026-04-29 13:09:31 +0700

    initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const  {defineConfig} = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://dev.profteam.su",
+    viewportWidth: 1280,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
