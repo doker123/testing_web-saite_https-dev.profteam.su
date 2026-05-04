613eccea17ddc0345f369bba98413f2f94bfc809
Doker123 <dodo2larin@gmail.com>
2026-05-04 08:53:40 +0700

    Доделал все тесты которые требовались в Варианте 1.

--- cypress/e2e/vacancies/applyVacancy.cy.js
+++ cypress/e2e/vacancies/applyVacancy.cy.js
@@ -4,9 +4,6 @@ describe('Отклик на вакансию студентом', ()=> {
     });
     it('Отклик на интересующую вакансию студента', () => {
         cy.visit('/account/main');
-        // cy.get('.page-nav__role-block > .button').click();
-        // cy.get('.desktop-modal').should('be.visible');
-        // cy.get('.select-role-form > :nth-child(3)').click();
         cy.wait(2000);
         cy.visit('/vacancies');
         cy.get('input[label-text="Поиск"]')
@@ -18,10 +15,10 @@ describe('Отклик на вакансию студентом', ()=> {
             .click();
         cy.wait(3000);
         cy.get('.vacancy-header__name').should('be.visible').and('not.be.empty')
-        cy.get(':nth-child(2) > .vacancy-item__info-wrapper ' +
-            '> .vacancy-item__footer-wrapper > ' +
-            '.vacancy-footer > .vacancy-footer__button-wrapper > ' +
-            '.button__background-color-green')
+        cy.get(':nth-child(1) > .vacancy-item__info-wrapper > ' +
+            '.vacancy-item__footer-wrapper > .vacancy-footer > ' +
+            '.vacancy-footer__button-wrapper > .button__background-color-green')
+            .should('be.visible')
             .click();
         cy.log('Отклик на вакансию за студента успешно.')
         cy.wait(3000);
